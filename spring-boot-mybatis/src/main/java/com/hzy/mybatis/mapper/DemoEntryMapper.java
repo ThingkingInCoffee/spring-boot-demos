0a2dfb38fec2f43390c3e3ad4ceb66e6e1e0f6e1
hzy <huangzhongyao@opechk.com>
2021-04-09 16:15:32 +0800

    mybatis mapper

--- spring-boot-mybatis/src/main/java/com/hzy/mybatis/mapper/DemoEntryMapper.java
+++ spring-boot-mybatis/src/main/java/com/hzy/mybatis/mapper/DemoEntryMapper.java
@@ -1,10 +1,18 @@
 package com.hzy.mybatis.mapper;
 
 import com.hzy.mybatis.entry.DemoEntry;
+import com.hzy.mybatis.entry.SensitiveEntry;
 
 public interface DemoEntryMapper {
     // @Select 需要在使用注解方式时开启，xml方式时关闭
 //    @Select("SELECT * FROM demo_entry WHERE id = #{id}")
     DemoEntry selectByPrimaryKey(Integer id);
 
+    DemoEntry selectDemo(Integer id);
+
+    int insertDemo();
+
+    int updateDemo();
+
+    int deleteDemo();
 }
